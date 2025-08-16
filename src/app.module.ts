a1169c1b29389da138af731357e1da2c6884a192
Jignesh Patel <jignesh.p@fundly.ai>
2025-08-16 17:58:26 +0530

    Crud completed with nest js and graph ql

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { BookModule } from './book/book.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
   imports: [ 
     ConfigModule.forRoot({
       isGlobal: true
     }),
-    MongooseModule.forRoot(process.env.MONGO_URI!) 
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      playground: true
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI!),
+    BookModule 
   ],
   controllers: [AppController],
   providers: [AppService],
